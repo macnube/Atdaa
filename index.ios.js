7473a62f7a40243546228c99a376f43b5af68adc
Paul McCloud <paul.mccloud@gmail.com>
2017-03-05 01:16:55 -0300

    Updated index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,24 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Provider } from 'react-redux';
+import { AppRegistry, NavigatorIOS } from 'react-native';
+import configureStore from './configureStore';
+import Root from './Root';
+import bugsnag from './App/utils/bugsnag'
+
+const store = configureStore();
 
 export default class Atdaa extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    //this.client.notify(new Error('Test Error'));
+    return <Root store={store}/>
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Atdaa', () => Atdaa);
