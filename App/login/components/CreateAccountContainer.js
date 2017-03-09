159c6889af927fe4fc4b43c4d47a391d73fb45e1
Paul McCloud <paul.mccloud@gmail.com>
2017-03-09 17:46:49 -0300

    Cleaned up CreateAccountContainer

--- App/login/components/CreateAccountContainer.js
+++ App/login/components/CreateAccountContainer.js
@@ -5,66 +5,65 @@ import CreateEmailContainer from './CreateEmailContainer'
 
 class CreateAccountContainer extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoading: false,
-			error: '',
-		}
-	}
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: false,
+      error: ''
+    }
+  }
 
-	toCreateEmail() {
-		this.props.navigator.push({
-			title: 'Sign Up',
-			component: CreateEmailContainer,
-			passProps: { 
-				toDashboard: this.props.toDashboard,
-				setUserInfo: this.props.setUserInfo,
-			}
-		})
-	}
+  toCreateEmail () {
+    this.props.navigator.push({
+      title: 'Sign Up',
+      component: CreateEmailContainer,
+      passProps: {
+        toDashboard: this.props.toDashboard,
+        setUserInfo: this.props.setUserInfo
+      }
+    })
+  }
 
-	onFacebookCreateAccount(data) {
-		console.log("Creating WITH FACEBOOK");
-		this.setState({
-			isLoading: true,
-		})
+  onFacebookCreateAccount (data) {
+    this.setState({
+      isLoading: true
+    })
 
-		let token = data.credentials.token
-		api.signInFacebook(token)
-			.then( (data.) => {
-				console.log('data from firestack', data.);
-				const userInfo = {
-					id: data.user.uid,
-					email: data.user.email,
-				}
-				api.setLocalUserInfo(userInfo);
-				this.props.setUserInfo(userInfo);
-				this.props.toDashboard();
-			})
-			.catch( (error) => {
-				this.setState({
-					isLoading: false,
-					error: error.description
-				})
-				console.log("Facebook login failed with", error);
-			})
-	}
+    let token = data.credentials.token
+    api.signInFacebook(token)
+      .then((data) => {
+        console.log('data from firestack', data)
+        const userInfo = {
+          id: data.user.uid,
+          email: data.user.email
+        }
+        api.setLocalUserInfo(userInfo)
+        this.props.setUserInfo(userInfo)
+        this.props.toDashboard()
+      })
+      .catch((error) => {
+        this.setState({
+          isLoading: false,
+          error: error.description
+        })
+        console.log('Facebook login failed with', error)
+      })
+  }
 
-	onFacebookLogout() {
-  	console.log("logout successful");
-	}
+  onFacebookLogout () {
+    console.log('logout successful')
+  }
 
-	render() {
-		return (
-			<CreateAccount
-				error={this.state.error}
-				isLoading={this.state.isLoading}
-				onFacebookCreateAccount={this.onFacebookCreateAccount.bind(this)}
-				onFacebookLogout={this.onFacebookLogout.bind(this)}
-				toCreateEmail={this.toCreateEmail.bind(this)} />
-		)
-	}
+  render () {
+    return (
+      <CreateAccount
+        error={this.state.error}
+        isLoading={this.state.isLoading}
+        onFacebookCreateAccount={this.onFacebookCreateAccount.bind(this)}
+        onFacebookLogout={this.onFacebookLogout.bind(this)}
+        toCreateEmail={this.toCreateEmail.bind(this)} />
+    )
+  }
 }
 
 export default CreateAccountContainer
