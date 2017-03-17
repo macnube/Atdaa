49d3c35eb399e8334214cc052059fff2bd5cda4e
Paul McCloud <paul.mccloud@gmail.com>
2017-03-17 15:53:42 -0300

    Added codepush

--- index.ios.js
+++ index.ios.js
@@ -4,25 +4,25 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { AppRegistry, NavigatorIOS } from 'react-native';
-import configureStore from './configureStore';
-import Root from './Root';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { AppRegistry, NavigatorIOS } from 'react-native'
+import configureStore from './configureStore'
+import Root from './Root'
 import bugsnag from './App/utils/bugsnag'
+import CodePush from 'react-native-code-push'
 
-const store = configureStore();
+const store = configureStore()
 
-export default class Atdaa extends Component {
-
-  constructor(props){
-    super(props);
-  }
-
+class Atdaa extends Component {
   render() {
     //this.client.notify(new Error('Test Error'));
     return <Root store={store}/>
   }
 }
+
+Atdaa = CodePush(Atdaa)
+
+export default Atdaa
 
 AppRegistry.registerComponent('Atdaa', () => Atdaa);
