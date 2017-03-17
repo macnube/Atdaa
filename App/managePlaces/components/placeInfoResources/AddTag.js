047c8e831ff4f055fc4f159c5ccb623ca77dbf71
Paul McCloud <paul.mccloud@gmail.com>
2017-03-17 16:04:29 -0300

    Centered AddTag

--- App/managePlaces/components/placeInfoResources/AddTag.js
+++ App/managePlaces/components/placeInfoResources/AddTag.js
@@ -1,42 +1,42 @@
 import React from 'react'
 
 import {
-	View,
-	Text,
-	Image,
-	StyleSheet,
-	TouchableHighlight
-} from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native'
 
 import Icon from '../../../shared/Icon'
 
 const AddTag = (props) => {
-	const icon = {id: 'editTags', imageURI: 'editTags'}
-	return (
-		<TouchableHighlight
-			onPress={() => props.handlePress()}>
-			<View style={styles.container}>
-				<Icon icon={icon} style={{height: 20}} shadow={false} />
-				<Text style={styles.text}>Edit Tags</Text>
-			</View>
-		</TouchableHighlight>
-	)
+  const icon = {id: 'editTags', imageURI: 'editTags'}
+  return (
+    <TouchableHighlight
+      onPress={() => props.handlePress()}>
+      <View style={styles.container}>
+        <Icon icon={icon} style={{height: 20}} shadow={false} />
+        <Text style={styles.text}>Edit Tags</Text>
+      </View>
+    </TouchableHighlight>
+  )
 }
 
-export default AddTag;
+export default AddTag
 
 AddTag.propTypes = {
-	handlePress: React.PropTypes.func.isRequired,
+  handlePress: React.PropTypes.func.isRequired
 }
 
 var styles = StyleSheet.create({
-	container: {
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		height: 45,
-	},
-	text: {
-		fontSize: 12,
-	}
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 45,
+    paddingLeft: 25
+  },
+  text: {
+    fontSize: 12
+  }
 })
