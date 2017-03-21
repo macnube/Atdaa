14abb0bf5fcca5cb99d6d62049a390b7895d1c36
Paul McCloud <paul.mccloud@gmail.com>
2017-03-21 18:52:58 -0300

    Fixed width

--- App/managePlaces/components/placeInfoResources/Address.js
+++ App/managePlaces/components/placeInfoResources/Address.js
@@ -1,40 +1,40 @@
 import React from 'react'
 
 import {
-	View,
-	Image,	
-	Text,
-	StyleSheet,
-	Dimensions
-} from 'react-native';
+  View,
+  Image,  
+  Text,
+  StyleSheet,
+  Dimensions
+} from 'react-native'
 
 const Address = (props) => {
-	const width = Dimensions.get('window').width;
-	//Additional 4 for elipsis
-	const containerWidth = width - 78
-	return (
-		<View style={[styles.container, {width: containerWidth - 15}]}>
-			<Image source={{uri: 'address'}} style={styles.image} />
-			<Text style={styles.text} numberOfLines={1}>{props.address}</Text>
-		</View>
-	)
+  const width = Dimensions.get('window').width
+  // Additional 4 for elipsis
+  const containerWidth = width - 78
+  return (
+    <View style={[styles.container, {width: containerWidth - 15}]}>
+      <Image source={{uri: 'address'}} style={styles.image} />
+      <Text style={styles.text} numberOfLines={1}>{props.address}</Text>
+    </View>
+  )
 }
 
 export default Address;
 
 var styles = StyleSheet.create({
-	container: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		marginLeft: 32,
-	},
-	image: {
-		width: 17,
-		height: 20,
-	},
-	text: {
-		fontSize: 12,
-		marginLeft: 15,
-		color: 'rgb(156,156,156)'
-	}
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 32,
+  },
+  image: {
+    width: 17,
+    height: 20,
+  },
+  text: {
+    fontSize: 12,
+    marginLeft: 15,
+    color: 'rgb(156,156,156)'
+  }
 })
--- App/managePlaces/components/placeInfoResources/OpenHours.js
+++ App/managePlaces/components/placeInfoResources/OpenHours.js
@@ -1,38 +1,41 @@
 import React from 'react'
 
 import {
-	View,
-	Text,
-	Image,
-	StyleSheet
-} from 'react-native';
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions
+} from 'react-native'
 
 const OpenHours = (props) => {
-	return (
-		<View style={styles.container}>
-			<Image source={{uri: 'openHours'}} style={styles.image} />
-			<Text style={styles.text}>Today</Text>
-			<Text style={styles.text}>{props.hours}</Text>
-		</View>
-	)
+  const width = Dimensions.get('window').width
+  const containerWidth = width - 78
+  return (
+    <View style={[styles.container, {width: containerWidth - 15}]}>
+      <Image source={{uri: 'openHours'}} style={styles.image} />
+      <Text style={styles.text}>Today</Text>
+      <Text style={styles.text} numberOfLines={1}>{props.hours}</Text>
+    </View>
+  )
 }
 
 export default OpenHours;
 
 var styles = StyleSheet.create({
-	container: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		marginLeft: 32,
-	},
-	image: {
-		width: 17,
-		height: 17,
-	},
-	text: {
-		fontSize: 12,
-		marginLeft: 15,
-		color: 'rgb(156,156,156)'
-	}
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 32,
+  },
+  image: {
+    width: 17,
+    height: 17,
+  },
+  text: {
+    fontSize: 12,
+    marginLeft: 15,
+    color: 'rgb(156,156,156)'
+  }
 })
